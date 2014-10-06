6e4e50e69b54163351f34a308e556469f9349ffd
Chris Cherry <ctcherry@gmail.com>
2014-10-06 14:11:32 -0700

    Output namespace declaration only once

--- index.js
+++ index.js
@@ -8,30 +8,40 @@ var PLUGIN_NAME = 'gulp-template-compile';
 
 module.exports = function (options) {
 	options = options || {};
+	
+	var headerOutput = false;
+	
+	// Build namespaces
+	var namespace = options.namespace || 'JST';
+	var namespaceList = namespace.split('.');
+	var namespaceLevels = [];
+	var namespaceDecls = [];
+
+	for(var n = 1; n <= namespaceList.length; n++) {
+		var tree = namespaceList.slice(0,n);
+		var buffer = tree.map(function(elm) {
+			return '["'+elm+'"]';
+		}).join('');
+		namespaceLevels.push(buffer);
+		namespaceDecls.push('window'+buffer+' = window'+buffer+' || {};');
+	}
+
+	// This will be the namespace path that the templates are defined on
+	var finalNamespace = namespaceLevels[namespaceLevels.length-1];
+	
+	// Output the namespace construction/declaration
+	function namespaceHeader() {	
+		return namespaceDecls.join(os.EOL)+os.EOL;
+	}
 
 	function compiler (file) {
-		var name = typeof options.name === 'function' && options.name(file) || file.relative;
-		var namespace = options.namespace || 'JST';
-		var namespaceList = namespace.split('.');
-		var namespaceLevels = [];
-		var namespaceDecls = [];
 
-		for(var n = 1; n <= namespaceList.length; n++) {
-			var tree = namespaceList.slice(0,n);
-			var buffer = tree.map(function(elm) {
-				return '["'+elm+'"]';
-			}).join('');
-			namespaceLevels.push(buffer);
-			namespaceDecls.push('window'+buffer+' = window'+buffer+' || {};'+os.EOL);
-		}
-		var finalNamespace = namespaceLevels[namespaceLevels.length-1];
+		var name = typeof options.name === 'function' && options.name(file) || file.relative;
 
 		var NSwrapper = '(function() {'+os.EOL;
-		NSwrapper += namespaceDecls.join('');
 		NSwrapper += 'window'+finalNamespace+'["'+ name.replace(/\\/g, '/') +'"] = ';
-
+		
 		var template = tpl(file.contents.toString(), false, options.templateSettings).source;
-
 		return NSwrapper + template + '})();';
 	}
 
@@ -51,8 +61,16 @@ module.exports = function (options) {
 
 		try {
 			var compiled = compiler(file);
+			var content;
+			if (!headerOutput) {
+				content = namespaceHeader() + compiled;
+				headerOutput = true;
+			} else {
+				content = compiled;
+			}
 
-			file.contents = new Buffer(compiled);
+			file.contents = new Buffer(content);
+
 			file.path = gutil.replaceExtension(file.path, '.js');
 		} catch (err) {
 			this.emit('error', new PluginError(PLUGIN_NAME, err, {fileName: filePath}));
